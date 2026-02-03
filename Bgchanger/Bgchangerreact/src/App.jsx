201592064541578cf01d32bc9c26ae81323d6329
sanchitumrao <sanchitumrao@gmail.com>
2026-02-03 20:24:02 +0530

    bg changer project 03

---
+++ Bgchanger/Bgchangerreact/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+  const [color, setColor]=useState('olive');
+  // function changeColor(color){
+  //   setColor(color)
+  // }
+  return (
+    <div className='w-full h-screen duration-200 ' style={{backgroundColor:color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 insert-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg text-white'onClick={()=>setColor('red')}  style={{backgroundColor:'red'}}>Red</button>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg text-white'onClick={()=>setColor('green')} style={{backgroundColor:'green'}}>Green</button>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg text-white'onClick={()=>setColor('black')} style={{backgroundColor:'black'}}>Black</button>
+          <button className='outline-none px-4 py-1 rounded-full shadow-lg text-white'onClick={()=>setColor('blue')} style={{backgroundColor:'blue'}}>Blue</button>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
